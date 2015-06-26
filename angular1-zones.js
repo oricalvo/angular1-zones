560d9f1714df38b7a2241ca14dc15269c129bbf4
oric <ori.calvo@gmail.com>
2015-06-27 02:55:53 +0300

    first commit

---
+++ angular1-zones.js
@@ -0,0 +1,25 @@
+﻿angular.module("ngZone", []).run(["$rootScope", function ($rootScope) {
+    var scopePrototype = $rootScope.constructor.prototype;
+
+    var originalApply = scopePrototype.$apply;
+
+    var zoneOptions = {
+        afterTask: function () {
+            try {
+                $rootScope.$digest();
+            } catch (e) {
+                $exceptionHandler(e);
+                throw e;
+            }
+        }
+    };
+
+    scopePrototype.$apply = function () {
+        var scope = this;
+        var applyArgs = arguments;
+
+        zone.fork(zoneOptions).run(function () {
+            originalApply.apply(scope, applyArgs);
+        });
+    }
+}]);
